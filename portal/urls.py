05cb30dd7b0ba6cd5f37956f82a97a6cb458c2f9
MichaelAndrew Chan <michaelac978@gmail.com>
2022-12-09 23:54:57 +0700

    [DevOps] configure boto3 email sending

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from sso.utils import send_email
 
 
 app_name = 'portal'
 urlpatterns = [
     path('', lambda _: HttpResponse("NTUSU ITC")),
+    path('email_test', lambda _: JsonResponse(send_email(
+        'do-not-reply',
+        'mich0107@e.ntu.edu.sg',
+        'TESTING',
+        'some content...'
+    )))
 ]

41047c0c55404d85160a7ad94b829e8b3c86814d
MichaelAndrew Chan <michaelac978@gmail.com>
2022-12-10 15:58:07 +0700

    [DevOps] update gitignore & readme

--- SUITC_Backend/settings.py
+++ SUITC_Backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import boto3
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,13 +150,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-# Additional settings
-
-ses_client = boto3.client(
-    'ses',
-    region_name='ap-southeast-1',
-    aws_access_key_id='AKIASDA4XJS4KLMM4RV7',
-    aws_secret_access_key='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',
-)
--- portal/urls.py
+++ portal/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-from django.http import HttpResponse, JsonResponse
-from sso.utils import send_email
+from django.http import HttpResponse
 
 
 app_name = 'portal'
 urlpatterns = [
     path('', lambda _: HttpResponse("NTUSU ITC")),
-    path('email_test', lambda _: JsonResponse(send_email(
-        'do-not-reply',
-        'mich0107@e.ntu.edu.sg',
-        'TESTING',
-        'some content...'
-    )))
 ]

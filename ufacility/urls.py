ca32c886c0e28966841bd188bdd7f9f097966337
Bryan-BC <bryanchristopherbc88@gmail.com>
2022-12-28 22:44:32 +0800

    Add ufacility urls

--- ufacility/urls.py
+++ ufacility/urls.py
@@ -1,2 +1,14 @@
+from django.urls import path
+from . import views
+
+
 app_name = 'ufacility'
-urlpatterns = []
+urlpatterns = [
+    path("users/", views.UserView.as_view(), name="users"),
+    path("users/<int:user_id>/", views.UserDetailView.as_view(), name="user-detail"),
+    path("verifications/", views.VerificationView.as_view(), name="verifications"),
+    path("verifications/<int:verification_id>/", views.VerificationDetailView.as_view(), name="verification-detail"),
+    path("bookings/", views.BookingView.as_view(), name="bookings"),
+    path("bookings/<int:booking_id>/", views.BookingDetailView.as_view(), name="booking-detail"),
+    path("venues/", views.VenueView.as_view(), name="venues"),
+]

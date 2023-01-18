d53d6e20e7306d561018b2bb8305fa0b2b15295a
Bryan-BC <bryanchristopherbc88@gmail.com>
2023-01-18 23:32:24 +0700

    [Ufacility] Add tests for user_details

---
+++ ufacility/tests/test_user_details.py
@@ -0,0 +1,67 @@
+from rest_framework import status
+from ufacility.tests.base_test import BaseAPITestCase
+from rest_framework.reverse import reverse
+from sso.models import User
+from ufacility.models import UFacilityUser
+from rest_framework.test import APIClient
+
+
+class UfacilityUserDetailsTestCase(BaseAPITestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create_user(
+            display_name = "test user",
+            email = "testuser@e.ntu.edu.sg",
+            username = "testuser",
+            password = "somevalidpassword123$",
+        )
+        self.ufacilityuser = UFacilityUser.objects.create(
+            user = self.user,
+            is_admin = False,
+            status = "accepted",
+            cca = "su",
+            role = "member",
+        )
+        self.client = APIClient()
+
+    def test_get_user_details_success(self):
+        # Test request as admin
+        self.client0.force_authenticate(user = self.user0)
+        url = reverse('ufacility:user-detail', kwargs={"user_id": self.ufacilityuser.id})
+        resp = self.client0.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.data["user"], self.user.id)
+        self.assertEqual(resp.data["is_admin"], False)
+        self.assertEqual(resp.data["status"], "accepted")
+        self.assertEqual(resp.data["cca"], "su")
+        self.assertEqual(resp.data["role"], "member")
+
+        # Test request as the ufacility user itself
+        self.client.force_authenticate(user = self.user)
+        url = reverse('ufacility:user-detail', kwargs={"user_id": self.ufacilityuser.id})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.data["user"], self.user.id)
+        self.assertEqual(resp.data["is_admin"], False)
+        self.assertEqual(resp.data["status"], "accepted")
+        self.assertEqual(resp.data["cca"], "su")
+        self.assertEqual(resp.data["role"], "member")
+
+
+    def test_get_user_details_fail_unauthorized(self):
+        self.client2.force_authenticate(user = self.user2)
+        url = reverse('ufacility:user-detail', kwargs={"user_id": self.user.id})
+        resp = self.client2.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_user_details_fail_forbidden(self):
+        self.client1.force_authenticate(user = self.user1)
+        url = reverse('ufacility:user-detail', kwargs={"user_id": self.user.id})
+        resp = self.client1.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_user_details_fail_not_found(self):
+        self.client0.force_authenticate(user = self.user0)
+        url = reverse('ufacility:user-detail', kwargs={"user_id": 100})
+        resp = self.client0.get(url)
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)

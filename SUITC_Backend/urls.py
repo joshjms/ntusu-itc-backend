c747657a9b49ff8807c33e1ec6d2b93f1f0b51da
joshjms <83194022+joshjms@users.noreply.github.com>
2024-01-04 08:55:27 +0700

    remove unused imports

--- SUITC_Backend/urls.py
+++ SUITC_Backend/urls.py
@@ -20,8 +20,6 @@ from django.conf import settings
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
-from django.http import HttpResponse
-
 
 schema_view = get_schema_view(
     openapi.Info(

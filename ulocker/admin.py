bf3c200e8bf03079c134d6d5394fb565b750f0e0
Bryan Matthew <bryanmatthew206@gmail.com>
2024-02-18 01:43:44 +0800

    [Ulocker] run migration

--- ulocker/admin.py
+++ ulocker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from ulocker.models import Location, Booking, ULockerAdmin
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Booking)
+admin.site.register(ULockerAdmin)

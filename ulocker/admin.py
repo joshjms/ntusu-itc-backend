ce463025d58d26e5f10ffb11b6426df96b8b8e69
Bryan Matthew <bryanmatthew206@gmail.com>
2024-02-18 01:54:38 +0800

    [ulocker] register model in admin

--- ulocker/admin.py
+++ ulocker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from ulocker.models import Location, Booking, ULockerAdmin
+from ulocker.models import Location, Booking, ULockerAdmin, Locker
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Booking)
 admin.site.register(ULockerAdmin)
+admin.site.register(Locker)

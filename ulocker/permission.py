f8d7a885c93e2819917a24d5e588638c4ad14ec7
Bryan Matthew <bryanmatthew206@gmail.com>
2024-03-05 23:51:26 +0700

    feat: GET /ulocker/booking/admin/ endpoint

---
+++ ulocker/permission.py
@@ -0,0 +1,14 @@
+from .models import ULockerAdmin
+from rest_framework.permissions import IsAuthenticated
+
+class IsULockerAdmin(IsAuthenticated):
+    message = 'ULocker Admin Required'
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        try:
+            return ULockerAdmin.objects.get(user=request.user)
+        except ULockerAdmin.DoesNotExist:
+            return False
--- ulocker/views.py
+++ ulocker/views.py
@@ -4,6 +4,7 @@ from .serializers import BookingSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import generics
 from rest_framework.response import Response
+from .permission import IsULockerAdmin
 
 # GET and POST /ulocker/booking/
 class UserBookingListView(generics.ListCreateAPIView):
@@ -15,4 +16,21 @@ class UserBookingListView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Booking.objects.filter(user=self.request.user)
+    
+# GET /ulocker/booking/admin/ for admin only
+class AdminBookingListView(generics.ListAPIView):
+    serializer_class = BookingSerializer
+    permission_classes = [IsULockerAdmin]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['applicant_name', 'matric_no', 'organization_name']
+    ordering_fields = ['creation_date', 'start_month', 'end_month', 'duration', 'organization_name', 'applicant_name']
+
+    def get_queryset(self):
+        queryset = Booking.objects.all()
+
+        for key, value in self.request.query_params.items():
+            if key in ['creation_date', 'start_month', 'end_month', 'duration', 'organization_name', 'applicant_name']:
+                queryset = queryset.filter(**{key: value})
+
+        return queryset
     

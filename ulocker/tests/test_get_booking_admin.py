04e469c4be5ecf37f75c421473caae5c1180f2e9
Bryan Matthew <bryanmatthew206@gmail.com>
2024-04-07 12:02:07 +0800

    feat: create test for /GET booking_admin

--- ulocker/tests/test_get_booking_admin.py
+++ ulocker/tests/test_get_booking_admin.py
@@ -9,7 +9,7 @@ class AdminBookingListViewTestCase(BaseAPITestCase):
         self.booking1 = Booking.objects.create(
             user=self.user0,
             locker=self.locker1,
-            applicant_name="Bob",
+            applicant_name="Mario123",
             matric_no="U11112222",
             phone_no="98765432",
             organization_name="NTU",
@@ -40,17 +40,60 @@ class AdminBookingListViewTestCase(BaseAPITestCase):
         self.assertIsInstance(response.data, list)
         self.assertEqual(len(response.data), 2)
 
-    def test_get_booking_admin_unauthenticated_fail(self):
+    def test_search_by_applicant_name_success(self):
         self.client1.force_authenticate(user=self.user1)
 
+        url = reverse('ulocker:admin_booking_list')
+        response = self.client1.get(url, {'search': 'Mario'})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_search_by_applicant_name_empty(self):
+        self.client1.force_authenticate(user=self.user1)
+
+        url = reverse('ulocker:admin_booking_list')
+        response = self.client1.get(url, {'search': 'Marioasdfasdfasdf'})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    def test_order_by_creation_date(self):
+        self.client1.force_authenticate(user=self.user1)
+
+        url = reverse('ulocker:admin_booking_list')
+        response = self.client1.get(url, {'ordering': 'creation_date'})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)  
+
+    def test_order_by_start_month(self):
+        self.client1.force_authenticate(user=self.user1)
+
+        url = reverse('ulocker:admin_booking_list')
+        response = self.client1.get(url, {'ordering': 'start_month'})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_booking_admin_unauthenticated_fail(self):
         url = reverse('ulocker:admin_booking_list')
         response = self.client1.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_booking_nonadmin_authenticated_fail(self):
+        self.client0.force_authenticate(user=self.user0)
+
         url = reverse('ulocker:admin_booking_list')
         response = self.client0.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_booking_nonadmin_authenticated_fail(self):
+        url = reverse('ulocker:admin_booking_list')
+        response = self.client0.get(url)
+
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
         

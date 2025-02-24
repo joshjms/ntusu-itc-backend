811c231fcf5119d2256401129aa8512233658904
ngvanthanggit <thangitcbg@gmail.com>
2025-02-24 23:18:55 +0800

    feat: item-loaning request api

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from .views import (
     ItemListView,
+    ItemDetailView,
     ItemLoanRequestListView,
 )
 
 app_name = 'inventory'
 urlpatterns = [
     path('items/', ItemListView.as_view(), name='item-list'),
+    path('item/<int:item_id>', ItemDetailView.as_view(), name='item-detail'),
+    path('loan-requests/', ItemLoanRequestListView.as_view(), name='loan-requests'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -18,5 +18,21 @@ from .serializers import (
 
 # view items
 class ItemListView(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    
+# view item with id
+class ItemDetailView(generics.RetrieveAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+    lookup_url_kwarg = 'item_id'
+    
+# view all loan requests
+class ItemLoanRequestListView(generics.ListAPIView):
+    serializer_class = ItemLoanRequestSerializer
+    queryset = ItemLoanRequest.objects.all()
+    
+# borrow item with id
+class BorrowItemView(generics.RetrieveAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()

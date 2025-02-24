4bd40322a67fcf9f85b2c40f7f297518ba41e91c
ngvanthanggit <thangitcbg@gmail.com>
2025-02-25 02:17:29 +0800

    feat: borrowing & returning items api

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,6 +4,8 @@ from .views import (
     ItemListView,
     ItemDetailView,
     ItemLoanRequestListView,
+    LoanRequestCreateView,
+    LoanRequestReturnView,
 )
 
 app_name = 'inventory'
@@ -11,4 +13,6 @@ urlpatterns = [
     path('items/', ItemListView.as_view(), name='item-list'),
     path('item/<int:item_id>', ItemDetailView.as_view(), name='item-detail'),
     path('loan-requests/', ItemLoanRequestListView.as_view(), name='loan-requests'),
+    path('items/loan/', LoanRequestCreateView.as_view(), name='loan-request-create'),
+    path('items/loan/return/<int:pk>', LoanRequestReturnView.as_view(), name='loan-request-return'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import status
+from rest_framework import generics, viewsets
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from django.utils import timezone
 
 from .models import (
     InventoryUser, 
@@ -32,7 +37,52 @@ class ItemLoanRequestListView(generics.ListAPIView):
     serializer_class = ItemLoanRequestSerializer
     queryset = ItemLoanRequest.objects.all()
     
-# borrow item with id
-class BorrowItemView(generics.RetrieveAPIView):
-    serializer_class = ItemSerializer
-    queryset = Item.objects.all()
+# send loan request
+class LoanRequestCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, *args, **kwargs):
+        item_id = request.data.get('item')
+        quantity = request.data.get('quantity')
+        user_id = request.user.id
+        
+        try:
+            item = Item.objects.get(pk=item_id)
+        except Item.DoesNotExist:
+            return Response({"Item not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if item.quantity < quantity:
+            return Response({"detail": "Not enough items available"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = ItemLoanRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            request_user = InventoryUser.objects.get(pk=user_id)
+            serializer.save(user=request_user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# return item
+class LoanRequestReturnView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def put(self, request, pk, *args, **kwargs):
+        try:
+            request_user = InventoryUser.objects.get(pk=request.user.id)
+            loan_request = ItemLoanRequest.objects.get(pk=pk, user=request_user)
+        except ItemLoanRequest.DoesNotExist:
+            return Response({"detail": "Loan request not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        loan_request.return_date = timezone.now()
+        loan_request.approval_status = 'returned'
+        loan_request.save()
+        
+        # increase item quantity
+        try:
+            item = Item.objects.get(pk=loan_request.item.id)
+            item.quantity += loan_request.quantity
+            item.save()
+        except Item.DoesNotExist:
+            return Response({"detail": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response({"detail": "Item returned successfully"}, status=status.HTTP_200_OK)

a3a665012bbd6fc57ebde5d87193e1f2b3d54d04
joshjms <joshjms1607@gmail.com>
2025-03-16 22:35:37 +0800

    feat: create retrieve update delete views for items
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
+from .views import ItemListCreateView, ItemDetailView
+
 app_name = 'inventory'
-urlpatterns = []
+
+urlpatterns = [
+    path('items/', ItemListCreateView.as_view(), name='item-list-create'),
+    path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Item
+from .serializers import ItemSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]  # Requires authentication
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user.inventorylender)
+
+class ItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user.inventorylender)

71fa36490b2572be6da4b65b959e31a4ff3ff8ba
ngvanthanggit <thangitcbg@gmail.com>
2025-03-23 03:08:08 +0800

    feat: add unit tests for inventory views

--- inventory/urls.py
+++ inventory/urls.py
@@ -11,9 +11,9 @@ from .views import (
 
 app_name = 'inventory'
 urlpatterns = [
-    path('items/', ItemListView.as_view(), name='item-list'),
+    path('items/', ItemListView.as_view(), name='items-list'),
     path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
-    path('loan-requests/', ItemLoanRequestListView.as_view(), name='loan-requests'),
+    path('loan-requests/', ItemLoanRequestListView.as_view(), name='loan-requests-list'),
     path('loan-requests/<str:username>/', UserLoanRequestListView.as_view(), name='user-loan-requests'),
     path('items/loan/', LoanRequestCreateView.as_view(), name='loan-request-create'),
     path('loan-requests/return/<int:pk>', LoanRequestReturnView.as_view(), name='loan-request-return'),

daa500570710acaef8d051610a9783e67b9288a1
joshjms <joshjms1607@gmail.com>
2025-07-31 01:27:11 +0800

    refactor(inventory): Move models into models directory
    Signed-off-by: joshjms <joshjms1607@gmail.com>

---
+++ inventory/models/__init__.py
@@ -0,0 +1,11 @@
+from .booking import InventoryBooking
+from .item import InventoryItem
+from .lender import InventoryLender
+from .user import InventoryUser
+
+__all__ = [
+    "InventoryBooking",
+    "InventoryItem",
+    "InventoryLender",
+    "InventoryUser",
+]
